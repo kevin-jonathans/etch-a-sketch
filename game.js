30ced8f4818ce408b24476a0410f94a9734c557a
kevin-jonathans <kevinjonathans@proton.me>
2022-10-26 00:52:59 +0700

    Add function to prepare the canvas

--- game.js
+++ game.js
@@ -1,1 +1,24 @@
-console.log("test");
+function prepareCanvas(size) {
+    for (let i = 0; i < size; i++) {
+        const canvasRow = document.createElement("div");
+        canvasRow.setAttribute("class", "row");
+        canvas.appendChild(canvasRow);
+        prepareBlock(size, canvasRow);
+    }
+}
+
+function prepareBlock(size, canvasRow) {
+    for (let i = 0; i < size; i++) {
+        const block = document.createElement("div");
+        const blockSize = canvas.clientWidth / size;
+        block.setAttribute("class", "block");
+        block.style.width = `${blockSize}px`;
+        block.style.height = `${blockSize}px`;
+
+        canvasRow.appendChild(block);
+    }
+
+}
+
+const canvas = document.querySelector(".canvas");
+prepareCanvas(10);
